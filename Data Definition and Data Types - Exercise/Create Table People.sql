147238d7c22619f6dad3e70d626d9981ccedf36c
Lubomir Pandurski <108551504+LubomirPandurski@users.noreply.github.com>
2022-09-21 22:52:47 +0300

    Add files via upload

---
+++ Data Definition and Data Types - Exercise/Create Table People.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `exercise`;
+CREATE TABLE `people` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(200) NOT NULL,
+`picture` BLOB,
+`height` DOUBLE (10, 2),
+`weight` DOUBLE (10, 2),
+`gender` CHAR(1) NOT NULL,
+`birthdate` DATE NOT NULL,
+`biography` TEXT
+);
