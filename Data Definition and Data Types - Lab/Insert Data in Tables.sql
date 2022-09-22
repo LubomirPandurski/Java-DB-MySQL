49729f626e229f9ae6992f19760abcc799e4c326
Lubomir Pandurski <108551504+LubomirPandurski@users.noreply.github.com>
2022-09-22 23:18:41 +0300

    Add files via upload

---
+++ Data Definition and Data Types - Lab/Insert Data in Tables.sql
@@ -0,0 +1,16 @@
+USE `gamebar`;
+
+INSERT INTO `categories` (`id`, `name`)
+VALUES (1, "game1"),
+		(2, "game2"),
+		(3, "game3");
+        
+INSERT INTO `employees` (`id`, `first_name`, `last_name`)
+VALUES (1, "Gosho", "Petrov"),
+		(2, "Stefan", "Ivanov"),
+        (3, "Gancho", "Penev");
+        
+INSERT INTO `products` (`id`, `name`)
+VALUES (1, "Game1"),
+		(2, "Game2"),
+        (3, "Game3");
